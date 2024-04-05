5077c9ad4a75c7af8fd73a8fa18e1d473363d1e7
alxkon13 <138485951+alxkon13@users.noreply.github.com>
2024-04-05 11:58:35 +0300

    Update PlotsTrackLengthAndEnergy.cpp

--- UserTools/PlotsTrackLengthAndEnergy/PlotsTrackLengthAndEnergy.cpp
+++ UserTools/PlotsTrackLengthAndEnergy/PlotsTrackLengthAndEnergy.cpp
@@ -118,7 +118,7 @@ bool PlotsTrackLengthAndEnergy::Execute(){
     c4.SaveAs("resol_length.png");
 
     c5.cd();
-    deltaenergy.Draw():
+    deltaenergy.Draw();
     deltaenergy.SetStats(0);
     TLegend legend2(0.7,0.7,0.9,0.9);
     legend2.AddEntry(&deltaenergy, "#DeltaE / E= |E_{Reco}-E_{MC}|/E_{Reco}","l");
