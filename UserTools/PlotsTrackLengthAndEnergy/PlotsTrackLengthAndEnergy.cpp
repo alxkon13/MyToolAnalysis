78ddbc166c530da058f13807f7aae6a999f71ddb
alxkon13 <138485951+alxkon13@users.noreply.github.com>
2024-04-09 12:33:28 +0300

    Update PlotsTrackLengthAndEnergy.cpp

--- UserTools/PlotsTrackLengthAndEnergy/PlotsTrackLengthAndEnergy.cpp
+++ UserTools/PlotsTrackLengthAndEnergy/PlotsTrackLengthAndEnergy.cpp
@@ -121,7 +121,7 @@ bool PlotsTrackLengthAndEnergy::Execute(){
     c4.SaveAs("resol_length.png");
 
     string title;
-    title="Energy Relative Deviation %;#DeltaE/E (%%) | mean =" + ("%.4f %%, std = %.4f %%", deltaenergy.GetMean(), deltaenergy.GetStdDev());
+    title="Energy Relative Deviation %;#DeltaE/E (%%) | mean =" + std::to_string(deltaenergy.GetMean()) + "%" + std::to_string(deltaenergy.GetStdDev()) + "%";
   
     c5.cd();
     deltaenergy.Draw();
