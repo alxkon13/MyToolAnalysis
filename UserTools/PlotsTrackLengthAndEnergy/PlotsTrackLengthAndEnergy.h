f5236b005f1702b239ee012351b0afa6533ab587
alxkon13 <138485951+alxkon13@users.noreply.github.com>
2024-05-14 19:51:59 +0300

    Update PlotsTrackLengthAndEnergy.h

--- UserTools/PlotsTrackLengthAndEnergy/PlotsTrackLengthAndEnergy.h
+++ UserTools/PlotsTrackLengthAndEnergy/PlotsTrackLengthAndEnergy.h
@@ -30,6 +30,8 @@ class PlotsTrackLengthAndEnergy: public Tool {
 
  private:
 
+  std::ofstream csvfile;
+
 // verbosity levels: if 'verbosity' < this level, the message type will be logged.
 	int verbosity=1;
 	int v_error=0;
