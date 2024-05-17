9cf20acfac46167617d9da513408cf778c0e4301
alxkon13 <138485951+alxkon13@users.noreply.github.com>
2024-05-17 11:07:12 +0300

    Update PlotsTrackLengthAndEnergy.cpp

--- UserTools/PlotsTrackLengthAndEnergy/PlotsTrackLengthAndEnergy.cpp
+++ UserTools/PlotsTrackLengthAndEnergy/PlotsTrackLengthAndEnergy.cpp
@@ -57,7 +57,7 @@ bool PlotsTrackLengthAndEnergy::Execute(){
     //for specific event analysis  
     TH1D diffDirhist1("diffDirAbs deltaE<10%", "diffDirAbs histogram", 100, 0, 0);
     TH1D diffDirhist2("diffDirAbs deltaE>30%", "diffDirAbs", 100, 0, 0);
-    TH2D mrdRecohist("recoTrackLengthInMrd deltaE<10%", "recoTrackLengthInMrd vs E_{MC}; E_{MC} [MeV]; Reco TRack Length in MRD [cm]", 100, 0, 2000., 50, 0., 200.);
+    TH2D mrdRecohist("recoTrackLengthInMrd deltaE<10%", "recoTrackLengthInMrd vs MC Energy; E_{MC} [MeV]; Reco TRack Length in MRD [cm]", 100, 0, 2000., 50, 0., 200.);
   
     int k=0;
 
@@ -93,8 +93,7 @@ bool PlotsTrackLengthAndEnergy::Execute(){
       //for specific event analysis      
       if(abs(deltaE)<15){
           diffDirhist1.Fill(diffDirAbs);
-          mrdRecohist.Fill(trueMuonEnergy,recoTrackLengthInMrd);
-          std::cout<<recoTrackLengthInMrd<<",\n";}
+          mrdRecohist.Fill(trueMuonEnergy,recoTrackLengthInMrd);}
       else if(abs(deltaE)>30){
           diffDirhist2.Fill(diffDirAbs);
       }
