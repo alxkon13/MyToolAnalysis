2151d0fd6ce6800d5f5f5b3485ea75717dfd032f
alxkon13 <138485951+alxkon13@users.noreply.github.com>
2024-05-17 11:18:22 +0300

    Update PlotsTrackLengthAndEnergy.cpp

--- UserTools/PlotsTrackLengthAndEnergy/PlotsTrackLengthAndEnergy.cpp
+++ UserTools/PlotsTrackLengthAndEnergy/PlotsTrackLengthAndEnergy.cpp
@@ -57,7 +57,7 @@ bool PlotsTrackLengthAndEnergy::Execute(){
     //for specific event analysis  
     TH1D diffDirhist1("diffDirAbs deltaE<10%", "diffDirAbs histogram", 100, 0, 0);
     TH1D diffDirhist2("diffDirAbs deltaE>30%", "diffDirAbs", 100, 0, 0);
-    TH2D mrdRecohist("recoTrackLengthInMrd", "recoTrackLengthInMrd vs MC Energy, #DeltaE/E>30%; E_{MC} [MeV]; Reco TRack Length in MRD [cm]", 100, 0, 2000., 100, 0., 300.);
+    TH2D mrdRecohist("recoTrackLengthInMrd", "recoTrackLengthInMrd vs MC Energy, #DeltaE/E>30%; E_{MC} [MeV]; Reco TRack Length in MRD [cm]");
   
     int k=0;
 
