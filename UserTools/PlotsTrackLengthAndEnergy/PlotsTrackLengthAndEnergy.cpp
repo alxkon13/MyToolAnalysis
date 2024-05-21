37b7dd8ebd195f17a156ff11539b589790590bbd
alxkon13 <138485951+alxkon13@users.noreply.github.com>
2024-05-21 13:01:34 +0300

    Update PlotsTrackLengthAndEnergy.cpp

--- UserTools/PlotsTrackLengthAndEnergy/PlotsTrackLengthAndEnergy.cpp
+++ UserTools/PlotsTrackLengthAndEnergy/PlotsTrackLengthAndEnergy.cpp
@@ -210,12 +210,7 @@ bool PlotsTrackLengthAndEnergy::Execute(){
     legend3.AddEntry(&diffDirhist1,"recoTrackLengthInMrd for #DeltaE/E<=10%","l");
     legend3.AddEntry(&diffDirhist2,"recoTrackLengthInMrd for #DeltaE/E>=20%","l");
     legend3.Draw("Same");*/
-    TPaletteAxis *palette = (TPaletteAxis*)h_myHisto->GetListOfFunctions()->FindObject("palette");
-    palette->SetX1NDC(0.9);
-    palette->SetX2NDC(0.95);
-    palette->SetY1NDC(0.2);
-    palette->SetY2NDC(0.8);
-    c6.SaveAs("recoMRD.png");
+    c6.SaveAs("recoMRD.root");
 
     /*c7.cd();
     mrdRecohist.SetStats(0);
