a23ccddb275753683a54ba3ea19f89fdd6eb3c94
alxkon13 <138485951+alxkon13@users.noreply.github.com>
2024-06-20 10:06:16 +0300

    Update PlotsTrackLengthAndEnergy.cpp

--- UserTools/PlotsTrackLengthAndEnergy/PlotsTrackLengthAndEnergy.cpp
+++ UserTools/PlotsTrackLengthAndEnergy/PlotsTrackLengthAndEnergy.cpp
@@ -48,8 +48,8 @@ bool PlotsTrackLengthAndEnergy::Execute(){
   
     TH2D lengthhist("True_RecoLength", "Reco Track Length vs MC Track Length; MC Track Length [cm]; Reconstructed Track Length [cm]", 50, 0, 400., 50, 0., 400.);
     TH2D energyhist("True_Reco_Energy", "Reco Energy vs MC Energy;  E_{MC} [MeV]; E_{reco} [MeV]", 100, 0, 2000., 100, 0., 2000.);
-    TH1D lengthresol1("wDNNRecolength", "Length Resolution;#DeltaR [cm]", 80, 0, 500);
-    TH1D lengthresol2("wlambda_max", "Length Resolution", 80, 0, 500);
+    TH1D lengthresol1("wDNNRecolength", "Length Resolution;#DeltaR [cm]", 80, 0, 1000);
+    TH1D lengthresol2("wlambda_max", "Length Resolution", 80, 0, 1000);
     TH1D energyresol1("MC Energy", "Energy Resolution;Energy [MeV]", 100, 0, 0);
     TH1D energyresol2("BDT Energy", "Energy Resolution", 100, 0, 0);
     TH1D deltaenergy("Relative Error", "Energy Relative Error %;#DeltaE/E (%)", 100, 0, 0); 
@@ -136,7 +136,7 @@ bool PlotsTrackLengthAndEnergy::Execute(){
     legend1.AddEntry(&lengthresol2,"#DeltaR = |L_{estimation}-L_{MC}| [cm]","l");
     legend1.AddEntry((TObject*)0, TString::Format("mean = %.2f, std = %.2f, Prev: mean = %.2f, std = %.2f ", lengthresol1.GetMean(),lengthresol1.GetStdDev(),lengthresol2.GetMean(),lengthresol2.GetStdDev()), "");
     legend1.Draw("Same");
-    c4.SaveAs("resol_length.root");
+    c4.SaveAs("resol_length.png");
 
     double meanDeltaE = deltaenergy.GetMean();
     std::stringstream meandeltaE;
